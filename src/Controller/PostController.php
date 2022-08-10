e6827e7d4b5aecc55eeeb7c25ceabff8aab81d1d
saeid <saeid.raei@hotmail.com>
2022-08-10 19:37:12 +0430

    add the controller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/post', name: 'post_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PostController.php',
+        ]);
+    }
+}
